97e5f4c3ddcafa9b11507fc303198de466d7034f
amolkedari92 <amolkedari92@gmail.com>
2020-07-12 09:52:20 +0530

    Axios network fallback added

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -4,28 +4,33 @@ import {
     BASE_API
 } from "../constants/";
 
+let responseBackup = null;
+
 class AxiosService {
     get = async (url, params = {}, successCb, errorCb) => {
-        let resp = await axios({
-            method: 'get',
-            url,
-            params,
-            headers: {
-                "Accept": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            },
-            baseURL: BASE_API
-        });
-
         try {
+            let resp = await axios({
+                method: 'get',
+                url,
+                params,
+                headers: {
+                    "Accept": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+            });
             if (resp.status === 200) {
+                responseBackup = resp.data;
                 successCb(resp.data);
             } else {
                 errorCb(resp);
             }
         } catch (e) {
             console.error(e);
-            errorCb({});
+            if(e.message === "Network Error") {
+                successCb(responseBackup);
+            } else {
+                errorCb({ error: e});
+            }
         }
     }
 };
